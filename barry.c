15318b624f3eff141116de5675100cd9f388780b
rubicks <naroza@gmail.com>
2016-02-06 20:21:43 +0000

    make it suck less

--- barry.c
+++ barry.c
@@ -1,17 +1,14 @@
 /* barry/barry.c */
 
-#if !HAVE_CONFIG_H
-#error "missing config.h"
-#endif
-
-#include "config.h"
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <ctype.h>
+#include <getopt.h>
 #include <string.h>
 
-#include <getopt.h>
+#include "config.h"
 
 #define WHEREAMI(STREAM)                                          \
   do {                                                            \
@@ -27,29 +24,24 @@
     }                     \
   } while (0) /**/
 
-bool
-goodc(char const c)
-{
-  return (('0' <= c) && (c <= '9')) || (('A' <= c) && (c <= 'Z')) ||
-         (('a' <= c) && (c <= 'z')) || ('_' == c);
-}
+#define MIN(X, Y) ((X) <= (Y) ? (X) : (Y))
 
-char *
-symbolize(char const *o)
-{
+static bool _goodc(char const c) { return isalnum(c) || ('_' == c); }
+
+static char *_symbolize(char const *str) {
   static char buf[64];
-  memset(buf, 0, 64);
-  for (size_t i = 0, n = 0x3f & strlen(o); i <= n; ++i) {
-    buf[i] = goodc(o[i]) ? o[i] : '_';
+  size_t i = 0;
+  size_t n = MIN(strlen(str), sizeof(buf) - 1);
+  memset(buf, 0, sizeof(buf));
+  for (; i < n; ++i) {
+    buf[i] = _goodc(str[i]) ? str[i] : '_';
   }
   return buf;
 }
 
-void
-print_help(FILE *f)
-{
+static void _usage(FILE *f, char *argv0) {
   fprintf(f, "\nUsage:");
-  fprintf(f, "\n  barry [option]... [<file>...]");
+  fprintf(f, "\n  %s [option]... [<file>...]", argv0);
   fprintf(f, "\n");
   fprintf(f, "\nOptions:");
   fprintf(f, "\n");
@@ -61,11 +53,10 @@ print_help(FILE *f)
   fprintf(f, "\n");
 }
 
-void
-readwrite(FILE *fin, FILE *fout, char *name)
-{
+static void _readwrite(FILE *fin, FILE *fout, char *name) {
   if (name) {
-    FAIL_UNLESS(0 <= fprintf(fout, "unsigned char const %s [] =\n", name));
+    FAIL_UNLESS(
+        0 <= fprintf(fout, "unsigned char const %s [] =\n", _symbolize(name)));
   }
 
   fprintf(fout, "{");
@@ -77,100 +68,71 @@ readwrite(FILE *fin, FILE *fout, char *name)
   fprintf(fout, " };\n");
 }
 
-int
-main(int const argc, char *const *argv)
-{
+int main(int argc, char **argv) {
   FILE *fout = stdout;
-
   char *name = NULL;
-
-  while (true) {
-    static struct option opts[] = {{"help", no_argument, 0, 'h'},
-                                   {"version", no_argument, 0, 'V'},
-                                   {"out", required_argument, 0, 'o'},
-                                   {"name", required_argument, 0, 'n'},
-                                   {0, 0, 0, 0}};
-
-    /* getopt_long stores the option index here. */
-    int optidx = 0;
-
-    int c = getopt_long(argc, argv, "Vho:n:", opts, &optidx);
-
-    /* printf( "optidx == %d\n", optidx ); */
-
-    /* Detect the end of the options. */
-    if (-1 == c) {
-      break;
-    }
-
+  int c = 0;
+  int optidx = 0;
+  static struct option opts[] = {{"help", no_argument, 0, 'h'},
+                                 {"version", no_argument, 0, 'V'},
+                                 {"out", required_argument, 0, 'o'},
+                                 {"name", required_argument, 0, 'n'},
+                                 {0, 0, 0, 0}};
+  while (-1 != (c = getopt_long(argc, argv, "Vho:n:", opts, &optidx))) {
     switch (c) {
-    case 0:
-      /* If this option set a flag, do nothing else now. */
-      if (0 != opts[optidx].flag) {
+      case 0:
+        /* If this option set a flag, do nothing else now. */
+        if (0 != opts[optidx].flag) {
+          break;
+        }
+        printf("option %s", opts[optidx].name);
+        if (optarg) {
+          printf(" with arg %s", optarg);
+        }
+        printf("\n");
+        break;
+      case 'V':
+        printf("%s\n", "barry v0.2.1");
+        return EXIT_SUCCESS;
+      case 'h':
+        _usage(stdout, argv[0]);
+        return EXIT_SUCCESS;
+      case 'o': {
+        FAIL_UNLESS(fout = fopen(optarg, "w"));
         break;
       }
-      printf("option %s", opts[optidx].name);
-      if (optarg) {
-        printf(" with arg %s", optarg);
+      case 'n': {
+        size_t len = 1 + strlen(optarg);
+        FAIL_UNLESS(name = malloc(len));
+        FAIL_UNLESS(memcpy(name, optarg, len));
+        break;
       }
-      printf("\n");
-      break;
-
-    case 'V':
-      printf("%s\n", PACKAGE_STRING);
-      return EXIT_SUCCESS;
-
-    case 'h':
-      print_help(stdout);
-      return EXIT_SUCCESS;
-
-    case 'o':
-      FAIL_UNLESS(fout = fopen(optarg, "w"));
-      break;
-
-    case 'n': {
-      size_t n = strlen(optarg);
-      FAIL_UNLESS(name = (char *)malloc(1 + n));
-      FAIL_UNLESS(memcpy(name, optarg, 1 + n));
-      break;
-    }
-
-    default:
-      fprintf(stderr, "getopt_long() returned '%c'", c);
-    case '?':
-      /* getopt_long already printed an error message. */
-      print_help(stderr);
-      return EXIT_FAILURE;
+      default:
+        fprintf(stderr, "getopt_long() returned '%c'", c);
+      case '?':
+        /* getopt_long already printed an error message. */
+        _usage(stderr, argv[0]);
+        return EXIT_FAILURE;
     }
   }
-
-  size_t non_opt_args = 0;
-
-  for (int i = optind, n = argc; i < n; ++i) {
-    ++non_opt_args;
-
-    if (0 == strcmp("-", argv[i])) {
-      /* do explicit stdin */
-      readwrite(stdin, fout, name);
-    } else {
-      /* do named file */
-      FILE *fin;
-      FAIL_UNLESS(fin = fopen(argv[i], "r"));
-      readwrite(fin, fout, symbolize(argv[i]));
-      FAIL_UNLESS(0 == fclose(fin));
+  if (optind < argc) {
+    for (int i = optind, n = argc; i < n; ++i) {
+      if (!strcmp("-", argv[i])) {
+        /* do explicit stdin */
+        _readwrite(stdin, fout, name);
+      } else {
+        /* do named file */
+        FILE *fin;
+        FAIL_UNLESS(fin = fopen(argv[i], "r"));
+        _readwrite(fin, fout, argv[i]);
+        FAIL_UNLESS(0 == fclose(fin));
+      }
     }
+  } else {
+    _readwrite(stdin, fout, name);
   }
-
-  if (0 == non_opt_args) {
-    readwrite(stdin, fout, name);
-  }
-
-  if (name) {
-    free(name);
-    name = NULL;
-  }
-
+  free(name);
+  name = NULL;
   FAIL_UNLESS(0 == fclose(fout));
-
   return EXIT_SUCCESS;
 }
